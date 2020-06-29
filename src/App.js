9c2ee477aa4325ccdab77d60051b1d0ce53ae950
Micah Lund <micah.r.lund@gmail.com>
2020-06-29 16:44:32 -0600

    Wrote a backspace function to remove the last character that was pressed.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,12 @@ class App extends Component {
     }
   }
 
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  }
+
   render() {
     return (
       <div>
